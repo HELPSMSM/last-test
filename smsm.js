361c30a10226a7072bd60b2c346e67054d7ef290
HELPSMSM <90980694+HELPSMSM@users.noreply.github.com>
2021-12-04 20:52:50 +0300

    Update smsm.js

--- smsm.js
+++ smsm.js
@@ -13,15 +13,27 @@ smsm.on("ready", () => {
 
 
 smsm.on("ready", () => {
-    console.log(`Im Online yeah' ${smsm.user.username}`)
-    smsm.user.setPresence({
-        status: 'online', //idle online dnd
-        activity: {
-            name: `اي شئ`,
-            type: "PLAYING",
-        }
-    })
+
+  console.log(`IM ONLINE YEAH`);
+
+  let statuses = [``];
+
+  setInterval(function() {
+
+    let PLAYING = statuses[Math.floor(Math.random() * statuses.length)];
+
+    jano14.user.setActivity(PLAYING, {
+      status: "dnd",
+      type: "playing",
+      
+
+
+    });
+
+  }, 2000);
+
 });
+
 
 smsm.login("OTE2Njg3MTUxNTExNTA3MDE2.Yatylw.IEBMTcoIH2g_e6ErCZLQhAsPeWg");
     
